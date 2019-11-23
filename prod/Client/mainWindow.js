780911fb1c92e4f944d00cf4e85ef04296ee6e02
SphericalPotatoInVacuum <a@bogdanov.co>
2019-11-23 19:42:32 +0300

    Added client gui and linked with server

---
+++ prod/Client/mainWindow.js
@@ -0,0 +1,119 @@
+const request = require('request');
+
+
+let slider;
+let textField;
+let backToMenu;
+let fileElem;
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.slider');
+    sessions_row = document.getElementById('sessions-row');
+    textField = document.getElementById('text-field');
+    backToMenu = document.getElementById('back-to-menu');
+    fileElem = document.getElementById('fileElem');
+    slider = instances = M.Slider.init(elems, {
+        duration: 500,
+        interval: 0,
+        indicators: false
+    })[0];
+    slider.pause();
+    backToMenu.addEventListener('click', () => {
+        fileElem.innerHTML = fileElem.innerHTML;
+        slider.set(0);
+        slider.pause();
+    });
+});
+
+let dropArea = document.getElementById("drop-area");
+dropArea.addEventListener('dragenter', handlerFunction, false)
+dropArea.addEventListener('dragleave', handlerFunction, false)
+dropArea.addEventListener('dragover', handlerFunction, false)
+dropArea.addEventListener('drop', handlerFunction, false)
+
+function handlerFunction() {}
+
+['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+    dropArea.addEventListener(eventName, preventDefaults, false)
+});
+
+function preventDefaults(e) {
+    e.preventDefault();
+    e.stopPropagation();
+}
+
+['dragenter', 'dragover'].forEach(eventName => {
+    dropArea.addEventListener(eventName, highlight, false);
+});
+
+['dragleave', 'drop'].forEach(eventName => {
+    dropArea.addEventListener(eventName, unhighlight, false);
+});
+
+function highlight(e) {
+    dropArea.classList.add('highlight');
+}
+
+function unhighlight(e) {
+    dropArea.classList.remove('highlight');
+}
+
+dropArea.addEventListener('drop', handleDrop, false)
+
+function handleDrop(e) {
+    let dt = e.dataTransfer
+    let files = dt.files
+    handleFiles(files)
+}
+
+function handleFiles(files) {
+    if (files.length > 1) {
+        alert('На данный момент поддерживается загрузка не более 1 файла :(');
+        return;
+    }
+    uploadFile(files[0]);
+}
+
+function uploadFile(file) {
+    console.log(file);
+    if (file.type != "text/plain") {
+        alert('Ты дебил? Я текст анализирую, нахер ты мне какую-то парашу тут суешь?! А ну быстро свалил с глаз моих и пока текстовый файл не притащишь, не возвращайся!');
+        return;
+    }
+    let url = 'http://127.0.0.1:1337/api/analyze_file'
+    let formData = new FormData();
+    formData.append('file', file);
+    fetch(url, {
+            method: 'POST',
+            body: formData
+        })
+        .then((res) => {
+            res.json()
+                .then((body) => {
+                    data = body.data;
+                    text = '<p>Оригинальный текст:</p>\n<p>';
+                    changed_text = '<p>'
+                    for (let [changed, word] of data) {
+                        console.log(changed, word);
+                        if (changed) {
+                            text += `<mark title="${word}">${word}</mark> `;
+                        } else {
+                            text += word + ' ';
+                        }
+                        changed_text += word + ' ';
+                    }
+                    text += '</p>\n<br>\n<br>\n<br>\n'
+                    text += '<p>Предлагаемый изменённый текст:</p>'
+                    text += changed_text;
+                    textField.innerHTML = text;
+                    slider.set(1);
+                    slider.pause();
+                })
+                .catch((e) => {
+                    alert('Всё сломалось')
+                });
+        })
+        .catch(() => {
+            /* Ошибка. Информируем пользователя */
+        });
+}
---
+++ prod/server.py
@@ -0,0 +1,44 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from json import dumps
+from flask_jsonpify import jsonify
+from datetime import datetime
+from hashlib import md5
+from os import urandom
+import werkzeug
+from prod import pipeline, production
+
+docs_c, wordset_c, full_c, docs_t, wordset_t, full_t, texts, fulls, sets, model, tfidf, fnames = pipeline()
+
+# Initialize flask app and api
+app = Flask(__name__)
+api = Api(app)
+
+file_index = 0
+
+
+class AnalyzeFile(Resource):
+    def post(self):
+        global file_index
+        args = file_parser.parse_args()
+        file = args['file']
+        print(file)
+        text = file.read().decode('utf-8')
+        text = text.replace('\n', '<br>')
+        file_index += 1
+        a = production(text, model, sets, fnames, tfidf, 2)
+        print(a)
+        return jsonify({'data': a})
+
+
+file_parser = reqparse.RequestParser()
+file_parser.add_argument(
+    'file',
+    type=werkzeug.datastructures.FileStorage,
+    location='files')
+
+
+api.add_resource(AnalyzeFile, '/api/analyze_file')
+
+if __name__ == '__main__':
+    app.run(port=1337)
